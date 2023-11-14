70586dc506a9a3f50bdde9652d2b9e14e01e6b91
Josias <josiasoliveira111@gmail.com>
2023-11-14 11:53:46 -0300

    pasta db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   BASE_URL: 'https://api.pokemontcg.io/v2/',
+  BASE_URL_LOCAL: 'http://localhost:3000/',
   API_KEY: 'da7db1e6-e234-4964-8a10-56195cca503b'
 };
